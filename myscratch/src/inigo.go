83b0bdc79c854eeb0fee56cc4eadac551bdb44df
mingchen <chenp02@gmail.com>
2020-06-11 16:52:11 -0400

    commit scratch

---
+++ myscratch/src/inigo.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(res, "Hello, my name is Paul Chen.")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	fmt.Println("Server is listening port 4000...")
+	http.ListenAndServe("localhost:4000", nil)
+}
