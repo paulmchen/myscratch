851f9f3db746f581c01d423f9ca95d5002710395
mingchen <chenp02@gmail.com>
2020-06-17 13:47:14 -0400

    compress files

---
+++ scratchpad/concurrency/compressx.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"compress/gzip"
+	"io"
+	"os"
+)
+
+func main() {
+	for _, file := range os.Args {
+		compress(file)
+	}
+	//var wg sync.WaitGroup
+}
+
+func compress(filename string) error {
+	in, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(filename + ".gz")
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	gzout := gzip.NewWriter(out)
+	_, err = io.Copy(gzout, in)
+	gzout.Close()
+
+	return err
+}
