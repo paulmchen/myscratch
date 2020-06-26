ac0c9059cf2fbf0ce6ef2e404564cf1b4f0dd5f3
mingchen <chenp02@gmail.com>
2020-06-26 15:00:33 -0400

    channel and goroutine leak

--- scratchpad/channel/src/channelclose.go
+++ scratchpad/channel/src/channelclose.go
@@ -5,29 +5,35 @@ import (
 	"time"
 )
 
-func send(ch chan string) {
-
+func send(ch chan<- string, done <-chan bool) {
 	for {
-		ch <- "hello"
-		time.Sleep(500 * time.Millisecond)
+		select {
+		case <-done:
+			fmt.Println("Done")
+			close(ch)
+			return
+		default:
+			ch <- "hello"
+			time.Sleep(500 * time.Millisecond)
+		}
 	}
 }
 
 func main() {
 	msg := make(chan string)
+	done := make(chan bool)
 	until := time.After(5 * time.Second)
 
-	go send(msg)
+	go send(msg, done)
 
 	for {
 		select {
 		case m := <-msg:
 			fmt.Println(m)
 		case <-until:
-			close(msg)
+			done <- true
 			time.Sleep(500 * time.Millisecond)
 			return
 		}
 	}
-
 }
